8136ece28aaa76a95cbd115f27e262a188e1a1a0
mofayez <mohammedfayez691@gmail.com>
2022-03-08 20:45:50 +0200

    First Commmit

---
+++ src/Updated/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Updated;
+
+/**
+ *
+ * @author Computers
+ */
+public class Main {
+
+    public static void main(String args[]) {
+
+        LoginFrame logFrame = new LoginFrame();
+        logFrame.setVisible(true);
+
+    }
+}
